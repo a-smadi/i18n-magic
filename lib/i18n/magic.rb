325c57122f3a9c910dadeb81b8fdda847fd8f419
Ali Smadi <alielsmadi@gmail.com>
2018-01-29 22:41:09 +0200

    set the basic CLI options and usage instructions

--- lib/i18n/magic.rb
+++ lib/i18n/magic.rb
@@ -1,11 +1,6 @@
-require "i18n/magic/version"
+require 'i18n/magic/version'
 
 module I18n
   module Magic
-    class Tester
-      def test
-        puts 'working..'
-      end
-    end
   end
 end
