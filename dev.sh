ffc7831d733f8f405ed78a55c172f37fbe2100ca
Ali Smadi <alielsmadi@gmail.com>
2018-03-20 23:36:10 +0200

    relocate vim window in tmux

--- dev.sh
+++ dev.sh
@@ -14,11 +14,11 @@ cd $I18N_MAGIC_DIR
 tmux new-session  -d -s i18n-magic
 tmux set-environment -t i18n-magic -g I18N_MAGIC_DIR $I18N_MAGIC_DIR
 
-tmux new-window     -t i18n-magic -n 'vim'
-tmux send-key       -t i18n-magic 'cd $I18N_MAGIC_DIR'      Enter 'vim .'                                                Enter
-
 tmux new-window     -t i18n-magic -n 'guard'
 tmux send-key       -t i18n-magic 'cd $I18N_MAGIC_DIR'      Enter 'bundle exec guard'                                    Enter
+
+tmux new-window     -t i18n-magic -n 'vim'
+tmux send-key       -t i18n-magic 'cd $I18N_MAGIC_DIR'      Enter 'vim .'                                                Enter
 
 if [ -z "$NESTED_TMUX" ]; then
   tmux -2 attach-session -t i18n-magic
