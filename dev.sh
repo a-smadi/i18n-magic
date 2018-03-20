c0b70cdf09bba667a74e9eb8d3a2bc06c7bd230c
Ali Smadi <alielsmadi@gmail.com>
2018-03-20 23:11:22 +0200

    add development environment script

---
+++ dev.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e # Exit on errors
+
+if [ -n "$TMUX" ]; then
+  export NESTED_TMUX=1
+  export TMUX=''
+fi
+
+if [ ! $I18N_MAGIC_DIR ]; then export I18N_MAGIC_DIR=$HOME/workspace/i18n-magic; fi
+
+cd $I18N_MAGIC_DIR
+
+tmux new-session  -d -s i18n-magic
+tmux set-environment -t i18n-magic -g I18N_MAGIC_DIR $I18N_MAGIC_DIR
+
+tmux new-window     -t i18n-magic -n 'vim'
+tmux send-key       -t i18n-magic 'cd $I18N_MAGIC_DIR'      Enter 'vim .'                                                Enter
+
+tmux new-window     -t i18n-magic -n 'specs'
+tmux send-key       -t i18n-magic 'cd $I18N_MAGIC_DIR'      Enter 'rake spec'                                            Enter
+
+if [ -z "$NESTED_TMUX" ]; then
+  tmux -2 attach-session -t i18n-magic
+else
+  tmux -2 switch-client -t i18n-magic
+fi
