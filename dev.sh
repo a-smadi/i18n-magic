56f89199bd8b6a2c09977e39141db0067573c1f4
Ali Smadi <alielsmadi@gmail.com>
2018-03-20 23:25:21 +0200

    add guard to the project's development dependencies

--- dev.sh
+++ dev.sh
@@ -17,8 +17,8 @@ tmux set-environment -t i18n-magic -g I18N_MAGIC_DIR $I18N_MAGIC_DIR
 tmux new-window     -t i18n-magic -n 'vim'
 tmux send-key       -t i18n-magic 'cd $I18N_MAGIC_DIR'      Enter 'vim .'                                                Enter
 
-tmux new-window     -t i18n-magic -n 'specs'
-tmux send-key       -t i18n-magic 'cd $I18N_MAGIC_DIR'      Enter 'rake spec'                                            Enter
+tmux new-window     -t i18n-magic -n 'guard'
+tmux send-key       -t i18n-magic 'cd $I18N_MAGIC_DIR'      Enter 'bundle exec guard'                                    Enter
 
 if [ -z "$NESTED_TMUX" ]; then
   tmux -2 attach-session -t i18n-magic
