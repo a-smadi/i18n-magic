516bb45e90ee2d4df035aba1dd439551eae13b9c
Ali Smadi <alielsmadi@gmail.com>
2018-03-23 12:28:54 +0200

    add console to dev script

--- dev.sh
+++ dev.sh
@@ -17,6 +17,9 @@ tmux set-environment -t i18n-magic -g I18N_MAGIC_DIR $I18N_MAGIC_DIR
 tmux new-window     -t i18n-magic -n 'guard'
 tmux send-key       -t i18n-magic 'cd $I18N_MAGIC_DIR'      Enter 'bundle exec guard'                                    Enter
 
+tmux new-window     -t i18n-magic -n 'console'
+tmux send-key       -t i18n-magic 'cd $I18N_MAGIC_DIR'      Enter 'bundle console'                                       Enter
+
 tmux new-window     -t i18n-magic -n 'vim'
 tmux send-key       -t i18n-magic 'cd $I18N_MAGIC_DIR'      Enter 'vim .'                                                Enter
 
