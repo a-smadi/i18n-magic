6194ed54c8d8459c79d960cc09a1f4bdb366b6ec
Ali Smadi <alielsmadi@gmail.com>
2018-04-06 18:03:01 +0300

    refactor environment helper specs

--- lib/i18n/magic/helpers/environment.rb
+++ lib/i18n/magic/helpers/environment.rb
@@ -2,7 +2,10 @@ module I18n
   module Magic
     module Helpers
       class Environment
-        def self.locales(dir_path = 'config/locales/')
+        LOCALE_FILES_PATH = 'config/locales/'.freeze
+        LOCALE_FILES_SAMPLE_PATH = 'spec/sample/config/locales/'.freeze
+
+        def self.locales(dir_path = LOCALE_FILES_PATH)
           locale_files = Dir["#{dir_path}*.yml"].select { |f| f =~ %r/\/[a-z]{2}.yml/ }
           locale_files.map { |f| f.remove(dir_path).remove('.yml').upcase }
         end
--- spec/i18n/magic/helpers/environment_spec.rb
+++ spec/i18n/magic/helpers/environment_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe I18n::Magic::Helpers::Environment do
   describe '#locales' do
     it 'returns an array of available locales within a project using yml translation files with 2 character names only' do
-      expect(I18n::Magic::Helpers::Environment.locales('spec/fixtures/files/config/locales/')).to match_array(%w[EN AR RU])
+      expect(I18n::Magic::Helpers::Environment.locales(I18n::Magic::Helpers::Environment::LOCALE_FILES_SAMPLE_PATH)).to match_array(%w[EN AR RU])
     end
   end
 end
