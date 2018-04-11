0753473ecfea5d01d1bb7b85efda3ddb245c44ce
Ali Smadi <alielsmadi@gmail.com>
2018-04-11 19:56:55 +0300

    fix indentation

--- spec/i18n/magic/helpers/environment_spec.rb
+++ spec/i18n/magic/helpers/environment_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe I18n::Magic::Helpers::Environment do
   describe '#locales' do
-  let(:locale_files_sample_path) { I18n::Magic::Helpers::Environment::LOCALE_FILES_SAMPLE_PATH }
+    let(:locale_files_sample_path) { I18n::Magic::Helpers::Environment::LOCALE_FILES_SAMPLE_PATH }
 
     it 'returns an array of available locales within a project using yml translation files with 2 character names only' do
       expect(I18n::Magic::Helpers::Environment.locales(locale_files_sample_path)).to match_array(%w[EN AR RU HI])
